54cc0dd88078d319a4d99e30ea04ec2038d14a7f
Nathi33 <nathi33.drnt@outlook.com>
2025-10-27 15:14:45 +0100

    add auto superuser creation for Render

--- maineblanc_project/wsgi.py
+++ maineblanc_project/wsgi.py
@@ -13,6 +13,20 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maineblanc_project.settings')
 
-import core.createsuperuser_script
-
 application = get_wsgi_application()
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
+    username = "Demo_admin_DP"
+    email = "admin_dp@test.com"
+    password = "Demo1234!"
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print("Superuser Demo_admin_DP créé avec succès !")
+    else:
+        print("Superuser Demo_admin_DP déjà existant.")
+except Exception as e:
+    print(f"Erreur lors de la création du superuser : {e}")
